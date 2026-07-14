1d4044eb23e7924a31834248ebc5786a9d7f49aa
Mainak Banerjee <90689364+mainak01-banerjee@users.noreply.github.com>
2026-07-14 12:26:31 +0530

    added query to create the addresses table.

--- mysql/queries.sql
+++ mysql/queries.sql
@@ -11,3 +11,15 @@ is_email_verified char(1) default 'N' check(is_email_verified in('Y','N')),
 is_active char(1) default 'N' check(is_active in('Y','N')),
 is_banned char(1) default 'N' check(is_banned in('Y','N'))
 );
+-- step 2 : create the address table
+create table addresses (
+addr_id int primary key,
+house_no varchar(20),
+floor_no varchar(3),
+street_name varchar(500) not null,
+land_mark varchar(100),
+user_id int,
+foreign key (user_id) references users(user_id)
+);
+-- note there is no city_id column in the address table becuase cities is not created yet and for it be the foreign key we need to reference the citites table.
+-- we will alter the table address to add the column city_id after we create the citites table.
