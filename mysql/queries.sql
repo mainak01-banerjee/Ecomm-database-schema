8961dc977b34148546b8a5920714bb9d1e8c028c
Mainak Banerjee <90689364+mainak01-banerjee@users.noreply.github.com>
2026-07-14 12:20:38 +0530

    created table users
    user is created without city_id column as cities table does not exist yet.we will add that later with alter table.

--- mysql/queries.sql
+++ mysql/queries.sql
@@ -1,1 +1,13 @@
-
+-- MYSQL queries for creating the data base schema
+create table users(
+user_id int primary key,
+user_name varchar(50) unique not null,
+email varchar(50) unique not null,
+phone int(10) unique not null,
+password varchar(255) unique not null,
+age int(2),
+user_type char(1) not null default 'U' check(user_type in('U','A')),
+is_email_verified char(1) default 'N' check(is_email_verified in('Y','N')),
+is_active char(1) default 'N' check(is_active in('Y','N')),
+is_banned char(1) default 'N' check(is_banned in('Y','N'))
+);
